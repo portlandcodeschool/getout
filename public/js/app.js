f9cb36534b2c3a4527070a78885195d728b624dc
Ondine Gallatin <ogallatin@gmail.com>
2015-04-23 12:12:13 -0700

    Comments to clarify jQuery for foundation

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
-// Foundation JavaScript
+// Foundation JavaScript - Required for Foundation JS components in js/foundation
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Backbone
