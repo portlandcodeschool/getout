0e622c73b32e29b16d986d81c12b3e30b9222f39
Robert Tribbia <rtribbia@gmail.com>
2015-04-23 20:12:07 -0700

    marker capability added

--- public/js/mapping.js
+++ public/js/mapping.js
@@ -1,49 +1,121 @@
-    
 var map;
-function initMap() {
-    console.log('2222');
-    var lat = 45.536500;
-    var lng = -122.648413;
-    var myCenter = new google.maps.LatLng(lat, lng);
-    function initialize() {
-      var mapOptions = {
-        zoom: 14,
-        center: myCenter
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'),
-          mapOptions);
-    }
+
+var lat = 45.536500;
+var lng = -122.648413;
+var markerArray = []; //stores Markers. When we wipe the map, we empty this array
 
 
+//----------------------------
+// Google Maps API code
+//----------------------------
 
-    function addMarker(lat,lng,desc) {
-      var infowindow = new google.maps.InfoWindow({
-          content: desc
-      });
-
-      var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(lat, lng),
-          map: map//,
-          //title: 'Uluru (Ayers Rock)'
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-      });
-    }
-
-
-    
-    function addResults(arr) {
-      arr.forEach(function (event) {
-        var desc = '<h2><a href="' + event.url[0] + '" target="_blank">' + event.title[0] + '</a></h2><br>';
-        desc += event.description + '<br>';
-        desc += '<strong>Time: </strong>' + event.start_time[0].split(' ')[1];
-        desc += '<br><strong>Venue: </strong>' + event.venue_name[0];
-        var eLat = Number(event.latitude);
-        var eLng = Number(event.longitude);
-        addMarker(eLat, eLng, desc);
-      });
-    }
+//Initialize map
+function initialize() {
+  console.log('MAPS!');
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(lat, lng)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 }
-    google.maps.event.addDomListener(window, 'load', initMap);	
-$(document).foundation();
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+//----------------------------
+// Google Maps API functions (mostly marker stuff)
+//----------------------------
+
+function addMarker(lat,lng,desc) {
+	var infowindow = new google.maps.InfoWindow({
+		content: desc
+	});
+
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(lat, lng),
+		animation: google.maps.Animation.DROP,
+		map: map
+	});
+
+	markerArray.push(marker);
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map,marker);
+	});
+}
+
+function clearMapMarkers() {
+	markerArray.forEach(function (m) {
+		m.setMap(null);
+	})
+	markerArray = [];
+
+}
+
+//----------------------------
+// BackBone Stuff
+//----------------------------
+
+
+//Event backbone model. Has 'renderToMap' function which just adds a marker to map.
+var eventModel = Backbone.Model.extend({
+	visible: false,
+	renderToMap: function() {
+		this.visible = true;
+		var desc = '<h1>' + this.attributes.title + '</h1>';
+		desc+= '<br><strong>Time: </strong>' + this.attributes.startTime;
+		desc+= '<br><strong>Venue: </strong>' + this.attributes.venueName;
+		desc+= '<br><a href="' + this.attributes.url + '" target="blank">More details ...';
+		addMarker(this.attributes.lat, this.attributes.lng,desc);
+	}
+});
+
+//Collection that holds markers.  pulls from server with .fetch() call in dateSelect function *below*
+var EventCollection = Backbone.Collection.extend({
+	model: eventModel,
+	url:'/api',
+	renderAll: function() {
+		clearMapMarkers();
+		this.each(function(m,i) {
+			m.renderToMap();
+		});
+		console.log('markers placed on map..');
+	}
+
+});
+
+function fetchSuccess(coll, res, opts) {
+	console.log($('#date').val() + ' EVENTS SUCCESSFULLY RECEIVED: ');
+	coll.renderAll();
+
+}
+
+function fetchError(coll, res, opts) {
+	console.log($('#date').val() + ' ERROR RECEIVING EVENTS ');
+}
+
+
+var coll = new EventCollection();
+
+
+//----------------------------
+// Rest of code
+//----------------------------
+function dateSelect() {
+	var selected = $('#date').val();
+	var fetchParam = { 
+		reset: true,
+		data: $.param({ date: selected}),
+		success: fetchSuccess,
+		error: fetchError
+	}
+	coll.fetch(fetchParam);
+}
+
+
+
+
+
+
+
+
+
