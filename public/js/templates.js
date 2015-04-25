cd8a18ab80003bb814e3934949175fe81f670de2
Ondine Gallatin <ogallatin@gmail.com>
2015-04-24 20:57:54 -0700

    Added template for categories

--- public/js/templates.js
+++ public/js/templates.js
@@ -10,14 +10,27 @@ app.templates.header = _.template(
 app.templates.map = _.template(
   '<div class="large-12 columns nopad">' + '<div id="map_canvas">' + '</div>' + '</div>');
 
+app.templates.categories = _.template(
+  '<select>' + 
+  '<option value="Art Galleries &amp; Exhibits"></option>' + 
+  '<option value="Concerts &amp; Tour Dates"></option>' + 
+  '<option value="Education"></option>' + 
+  '<option value="Food &amp; Wine"></option>' + 
+  '<option value="Health &amp; Wellness"></option>' + 
+  '<option value="Kids &amp; Family"></option>' + 
+  '<option value="Museums &amp; Attractions"></option>' + 
+  '<option value="Outdoors &amp; Recreation"></option>' + 
+  '<option value="Performing Arts"></option>' + 
+  '<option value="Sports"></option>' + 
+  '</select>');
+
 // #footerForm
 app.templates.footerForm = _.template(
   '<div class="large-12 columns">' + '<hr />' + '<h5>Find an event and get on out!</h5>' + '<form>' + 
 
   '<div class="row">' + 
-  '<div class="large-4 medium-4 columns">' + '<label>Select a date</label>' + '<select>' + '<option value="date"><%= Date %></option>' + '</select>' + '</div>' + 
-  '<div class="large-4 medium-4 columns">' + '<label>Select type of event</label>' + '<select>' + '<option value="activity"><%= Activity %></option>' + '</select>' + '</div>' + 
-  '<div class="large-4 medium-4 columns">' + '<label>Select a radius on the map</label>' + '<select>' + '<option value="distance"><%=  Distance %></option>' + '</select>' + '</div>' + 
+  '<div class="large-6 medium-6 columns">' + '<label>Select a date</label>' + '<select>' + '<option value="date"><%= Date %></option>' + '</select>' + '</div>' + 
+  '<div class="large-6 medium-6 columns">' + '<label>Select type of event</label>' + '<select>' + '<option value="activity"><%= Activity %></option>' + '</select>' + '</div>' + 
   '</form>' + '</div>');
 
 app.templates.dateItem = _.template(
@@ -25,9 +38,6 @@ app.templates.dateItem = _.template(
 
 app.templates.getoutItem = _.template(
   '<option value="activity"><%= Activity %></option>');
-
-app.templates.mapRadius = _.template(
-  '<option value="distance"><%= Distance %></option>');
 
 app.templates.footerButton = _.template(
   '<div class="large-12 columns">' + 
