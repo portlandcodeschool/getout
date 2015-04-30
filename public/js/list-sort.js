95764b2391615172b78dcb5fa34b34cddcb926ba
kristopher-hutchison <kristopher.hutchison@gmail.com>
2015-04-30 01:30:55 -0700

    new styles and html

---
+++ public/js/list-sort.js
@@ -0,0 +1,33 @@
+$( function() {
+
+  // init Isotope
+  var $container = $('.isotope').isotope({
+    layoutMode: 'vertical',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '.category',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
+  });
+
+  // bind sort button click
+  $('#sorts').on( 'click', 'button', function() {
+    var sortValue = $(this).attr('data-sort-value');
+    $container.isotope({ sortBy: sortValue });
+  });
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+});
